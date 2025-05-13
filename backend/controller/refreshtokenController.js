f3238f20e8061c3ab4bbf9e9615c7bd708d413e8
Siddhant Gawad <siddhantgawad4@gmail.com>
2025-05-13 13:06:26 -0400

    added rateLimiter

--- backend/controller/refreshtokenController.js
+++ backend/controller/refreshtokenController.js
@@ -2,6 +2,7 @@ import jwt from "jsonwebtoken";
 import redis from "../redis/redisClient.js";
 import dotenv from "dotenv";
 
+
 dotenv.config();
 
 const refreshtokenController = async function (req,res){
@@ -15,9 +16,21 @@ const refreshtokenController = async function (req,res){
         const decoded = jwt.verify(cookie,process.env.JWT_REFRESH_SECRET);
         const userId = decoded.userId;
 
+        //step 3: check if token matches the one stored in red
+        const storedToken = await redis.get(`refresh:${newUser._id}`);
+        if(!storedToken || storedToken !== cookie) return res.status(403).json({message:"Refresh token invalid or reused"});
+
+        //step 4: issue new token
+        const token = jwt.sign({userId: newUser._id},process.env.JWT_SECRET,{expiresIn:"15m"});
+
+        //step 5: return new token 
+        return res.status(200).json({token});
 
     }catch(err){
-
+        console.error("Refresh error:",err);
+        return res.status(500).json({message:"Internal server error"});
     }
     
-}
+};
+
+export default refreshtokenController;
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,22 +1,27 @@
 import express from "express";
+import {authRateLimiter} from "../middleware/rateLimiter.js"
 
 const app = express();
 const router = express.Router();
 
 //login
 import loginController from "../controller/account/loginController.js";
-router.post("/login",[
+router.post("/login",authRateLimiter,[
     body("username").notEmpty(),
     body("password").notEmpty()
 ],loginController);
 
 //register
 import registerController from "../controller/account/registerController.js";
-router.post("/register",[
+router.post("/register",authRateLimiter,[
     body("username").notEmpty(),
     body("password").notEmpty()
 ], registerController);
 
+// refresh 
+import refreshtokenController from "../controller/refreshtokenController.js";
+router.post("/refresh",refreshtokenController
+)
 
 
 
