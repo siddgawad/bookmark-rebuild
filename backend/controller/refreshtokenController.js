c00722af9435a3af062e32710df1c6da44ddfdea
Siddhant Gawad <siddhantgawad4@gmail.com>
2025-05-13 20:58:07 -0400

    rotated refresh token

--- backend/controller/refreshtokenController.js
+++ backend/controller/refreshtokenController.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 import redis from "../redis/redisClient.js";
 import dotenv from "dotenv";
-import user from "../models/userModel.js";
+
 
 
 dotenv.config();
@@ -21,11 +21,16 @@ const refreshtokenController = async function (req,res){
         const storedToken = await redis.get(`refresh:${userId}`);
         if(!storedToken || storedToken !== cookie) return res.status(403).json({message:"Refresh token invalid or reused"});
 
-        //step 4: issue new token
-        const token = jwt.sign({userId},process.env.JWT_SECRET,{expiresIn:"15m"});
+        //step 4: create new/rotate the refresh token 
+        const newrefreshToken = jwt.sign({ userId }, process.env.JWT_REFRESH_SECRET, { expiresIn: "7d" });
+        await redis.set(`refresh:${userId}`, newrefreshToken, "EX", 7 * 24 * 60 * 60);
+        res.cookie("refreshToken", newrefreshToken, {
+            httpOnly: true, secure: true, sameSite: "strict", maxAge: 7 * 24 * 60 * 60 * 1000 // milliseconds
+});
 
-        //step 5: return new token 
-        return res.status(200).json({token});
+const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "15m" });
+return res.status(200).json({ token });
+
 
     }catch(err){
         console.error("Refresh error:",err);
